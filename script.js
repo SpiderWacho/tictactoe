54b124bda873ae94729186e47e2b41843bb2a9bf
SpiderWacho <gastonvecch@gmail.com>
2021-10-25 00:39:51 -0300

    worked on css, also game now display who is the winner, and change who stars when rematch

--- script.js
+++ script.js
@@ -46,32 +46,48 @@ const gameBoard = (() => {
 const player = (symbol, name) => {
     const getSymbol = () => symbol;
     const getName = () => name;
-    return {getSymbol, getName};
+    function changeName (newName){ 
+         name = newName}
+    return {getSymbol, getName, changeName};
 }
 
 const gameState = (() => {
     let turn = 0;
     let players = 0;
+    let games = 0;
     let gameOver = false;
+    let tie = false;
     const introDiv = document.querySelector(".intro");
     const btnSinglePlayer = document.querySelector("#btnSinglePlayer");
     const btnTwoPlayer = document.querySelector("#btnTwoPlayer");
-    const game = document.querySelector(".game")
-    const submitBtn = document.querySelector("#submitNames")
+    const game = document.querySelector(".game");
+    const submitBtn = document.querySelector("#submitNames");
+    const rematch = document.querySelector("#rematch");
+    const changeMode = document.querySelector("#changeMode");
+    const winnerP = document.querySelector("#winner")
     
     
     submitBtn.addEventListener("click", function(e){
         e.preventDefault();
-        let firstPlayerName = document.getElementById('twoPlayer1Name').value;
-        let secondPlayerName = document.getElementById('twoPlayer2Name').value;
-        playerOne = player("x", firstPlayerName)
-        playerTwo = player("O", secondPlayerName)
-        introDiv.style.display = "none"; 
         gameStart();
     })
 
     function gameStart() {
+        let throwAway = "";
+        introDiv.style.display = "none"; 
+        document.querySelector("#endGame").style.display = "none";
         game.style.display = "block";
+        turn = 0;
+        let firstPlayerName = document.getElementById('twoPlayer1Name').value;
+        let secondPlayerName = document.getElementById('twoPlayer2Name').value;
+        if (games % 2 === 0) {
+            playerOne = player("x", firstPlayerName)
+            playerTwo = player("O", secondPlayerName)
+        }
+        else {
+            playerOne = player("x", secondPlayerName)
+            playerTwo = player("O", firstPlayerName)
+        }
         gameBoard.clearBoard();
     }
 
@@ -84,40 +100,55 @@ const gameState = (() => {
         }
     }
 
-    btnSinglePlayer.addEventListener("click", () => {players = 1, introDiv.style.display = "none", gameStart();})
+    btnSinglePlayer.addEventListener("click", () => {players = 1, document.querySelector("#singlePlayerName").style.borderBottom = "3px solid black"})
     btnTwoPlayer.addEventListener("click", () => {players = 2, document.querySelector(".divTwoPlayer").style.display = "flex"})
 
     
-    function checkForVictory() {
-        if (gameBoard.board[0].textContent === gameBoard.board[1].textContent && gameBoard.board[1].textContent === gameBoard.board[2].textContent && gameBoard.board[0].textContent != "") {
+    function isVictory() {
+        if (gameBoard.board[0].textContent === gameBoard.board[1].textContent
+             && gameBoard.board[1].textContent === gameBoard.board[2].textContent && gameBoard.board[0].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[3].textContent === gameBoard.board[4].textContent && gameBoard.board[4].textContent === gameBoard.board[5].textContent && gameBoard.board[3].textContent != "") {
+        else if (gameBoard.board[3].textContent === gameBoard.board[4].textContent
+             && gameBoard.board[4].textContent === gameBoard.board[5].textContent && gameBoard.board[3].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[6].textContent === gameBoard.board[7].textContent && gameBoard.board[7].textContent === gameBoard.board[8].textContent && gameBoard.board[6].textContent != "") {
+        else if (gameBoard.board[6].textContent === gameBoard.board[7].textContent
+             && gameBoard.board[7].textContent === gameBoard.board[8].textContent && gameBoard.board[6].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[0].textContent === gameBoard.board[3].textContent && gameBoard.board[3].textContent === gameBoard.board[6].textContent && gameBoard.board[0].textContent != "") {
+        else if (gameBoard.board[0].textContent === gameBoard.board[3].textContent
+             && gameBoard.board[3].textContent === gameBoard.board[6].textContent && gameBoard.board[0].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[1].textContent === gameBoard.board[4].textContent && gameBoard.board[4].textContent === gameBoard.board[7].textContent && gameBoard.board[1].textContent != "") {
+        else if (gameBoard.board[1].textContent === gameBoard.board[4].textContent
+             && gameBoard.board[4].textContent === gameBoard.board[7].textContent && gameBoard.board[1].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[2].textContent === gameBoard.board[5].textContent && gameBoard.board[5].textContent === gameBoard.board[8].textContent && gameBoard.board[2].textContent != "") {
+        else if (gameBoard.board[2].textContent === gameBoard.board[5].textContent
+             && gameBoard.board[5].textContent === gameBoard.board[8].textContent && gameBoard.board[2].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[0].textContent === gameBoard.board[4].textContent && gameBoard.board[4].textContent === gameBoard.board[8].textContent && gameBoard.board[0].textContent != "") {
+        else if (gameBoard.board[0].textContent === gameBoard.board[4].textContent
+             && gameBoard.board[4].textContent === gameBoard.board[8].textContent && gameBoard.board[0].textContent != "") {
             gameOver = true;
             return true;
         }
-        else if (gameBoard.board[2].textContent === gameBoard.board[4].textContent && gameBoard.board[4].textContent === gameBoard.board[6].textContent && gameBoard.board[2].textContent != "") {
+        else if (gameBoard.board[2].textContent === gameBoard.board[4].textContent 
+            && gameBoard.board[4].textContent === gameBoard.board[6].textContent && gameBoard.board[2].textContent != "") {
+            gameOver = true;
+            return true;
+        }
+        else if (gameBoard.board[0].textContent != "" && gameBoard.board[1].textContent != "" && gameBoard.board[2].textContent != "" 
+                && gameBoard.board[3].textContent != "" && gameBoard.board[4].textContent != "" && gameBoard.board[5].textContent != "" 
+                && gameBoard.board[6].textContent != "" && gameBoard.board[7].textContent != "" && gameBoard.board[8].textContent != "") {
+            tie = true;
             gameOver = true;
             return true;
         }
@@ -128,16 +159,30 @@ const gameState = (() => {
     }
     
     const nextTurn = () => {
-        if (!checkForVictory()){
-            //TODO: Game End
-            console.log(getCurrentPlayer().getName());
+        if (!isVictory()){
             turn++;
-            console.log(gameOver);
         }
         else {
-           document.querySelector("#endGame").style.display = "block";
+           endGame();
         }
-    };
+    }
+
+    function endGame() {
+        
+        document.querySelector("#endGame").style.display = "flex";
+        if (tie === false) {    
+            winnerP.textContent = `${getCurrentPlayer().getName()} is the winner`;
+        }
+        else {
+            console.log(`Its a tie!`);
+            tie = false;
+        }
+    }
+
+    rematch.addEventListener("click", function(){
+        games++;
+        gameStart();
+    })
     return {nextTurn, players, gameOver, getCurrentPlayer};
 })()
 
